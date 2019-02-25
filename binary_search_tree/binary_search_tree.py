95313cba4414c52d8123f1ccbf23f565eb6de7c8
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-25 15:34:49 -0700

    starto n binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,19 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        target = 'left' if value <= self.value else 'right'
+        target_val = getattr(self, target)
+        if target_val is None:
+            setattr(self, target, value)
+        else:
+            target_val.insert(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
