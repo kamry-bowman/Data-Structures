fab7c97e7fa5df89d999e93265fad45e2bad3075
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-25 13:44:51 -0700

    passing test for queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,64 @@
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+
+
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, val):
+        new_node = Node(val=val)
+
+        if self.tail is not None:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    def remove_head(self):
+
+        if self.head is not None:
+            old_head = self.head
+            self.head = old_head.next
+
+            if self.tail == old_head:
+                self.tail = None
+
+            return old_head.val
+
+        else:
+            return None
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        self.storage = Linked_List()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.size += 1
+        return self.storage.add_to_tail(item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        result = self.storage.remove_head()
+        if result is None:
+            self.size = 0
+        else:
+            self.size -= 1
+        return result
+
+    def len(self):
+        return self.size
+
+
+example = Queue()
+example.enqueue(1)
+example.enqueue(2)
+print(example.dequeue())
+print(example.dequeue())
+print(example.dequeue())
