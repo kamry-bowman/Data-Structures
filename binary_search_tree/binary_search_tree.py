b62f567e2a043d0ae23fad25252380668b758533
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-25 17:10:25 -0700

    finish up heap.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -8,12 +8,20 @@ class BinarySearchTree:
         target = 'left' if value <= self.value else 'right'
         target_val = getattr(self, target)
         if target_val is None:
-            setattr(self, target, value)
+            setattr(self, target, BinarySearchTree(value))
         else:
             target_val.insert(value)
 
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        if target < self.value:
+            return self.left and self.left.contains(target)
+        else:
+            return self.right and self.right.contains(target)
 
     def get_max(self):
-        pass
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,83 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        end = self.storage.pop()
+        if self.storage:
+            deleted = self.storage[0]
+            self.storage[0] = end
+            self._sift_down(0)
+            return deleted
+        else:
+            return end
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        if not self.storage:
+            return None
+        else:
+            return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        if index > 0:
+            storage = self.storage
+            parent_index = (index - 1) // 2
 
-  def _sift_down(self, index):
-    pass
+            if storage[index] > storage[parent_index]:
+                storage[index], storage[parent_index] = storage[parent_index], storage[index]
+                if parent_index > 0:
+                    self._bubble_up(parent_index)
+
+    def _sift_down(self, index):
+        storage = self.storage
+        l_child_i = index * 2 + 1
+        value = storage[index]
+        try:
+            l_child_val = storage[l_child_i]
+        except IndexError:
+            l_child_val = None
+        r_child_i = index * 2 + 2
+        try:
+            r_child_val = storage[r_child_i]
+        except IndexError:
+            r_child_val = None
+
+        # figure out if left or right is greater
+        if not r_child_val or l_child_val >= r_child_val:
+            greatest_child_i, greatest_child_val = l_child_i, l_child_val
+        else:
+            greatest_child_i, greatest_child_val = r_child_i, r_child_val
+
+        # figure out if value is left than lower than the greater of its children
+        if greatest_child_val is not None and value <= greatest_child_val:
+            storage[index], storage[greatest_child_i] = storage[greatest_child_i], storage[index]
+
+            # continue sift if child isn't terminal
+            if greatest_child_i < len(storage) - 1:
+                self._sift_down(greatest_child_i)
+
+
+heap = Heap()
+heap.insert(6)
+heap.insert(8)
+heap.insert(10)
+heap.insert(9)
+heap.insert(1)
+heap.insert(9)
+heap.insert(9)
+heap.insert(5)
+print(heap.get_size())
+print(heap.get_max())
+heap.delete()
+print(heap.get_size())
+print(heap.get_max())
+heap.delete()
+print(heap.get_size())
+print(heap.get_max())
